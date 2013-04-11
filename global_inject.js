2eecaba8f0a30c115e81dc9ed93fc4b462f61e90
Tarık Taha Uygun <ttuygun@yahoo.com>
2013-04-11 15:30:36 -0500

    Refined scrollability detection for APIs

--- global_inject.js
+++ global_inject.js
@@ -1,23 +1,23 @@
 /*global $ Message ScrollableMap */
 
-var SM = SM || {};
+var SM = SM || { count: 0 };
 SM.scriptInjected = false;
 
 document.addEventListener('load', function (event) {
     if (SM.scriptInjected) return;
     SM.scriptInjected = true;
 
-    SM.prepend(document.head, SM.injectScript('inject_content.js'));
+    document.head.insertBefore(SM.injectScript('inject_content.js'), document.head.firstChild);
 
     window.addEventListener('mapsFound', function (event) {
-        new ScrollableMap(event.detail, ScrollableMap.TYPE_API);
+        new ScrollableMap(event.detail, ScrollableMap.TYPE_API, SM.count++);
     }, false);
 }, true);
 
 Message.extension.addListener(function (action, data, sender, sendResponse) {
     switch (action) {
         case 'listenBodyScrolls':
-            if (window.top == window) {
+            if (window.top === window) {
                 // document.body.addEventListener('DOMSubtreeModified', SM.updateBodyScrolls, false);
                 window.addEventListener('resize', SM.updateBodyScrolls, false);
                 window.addEventListener('load', function () { setTimeout(SM.updateBodyScrolls, 1000); }, false);
@@ -39,12 +39,4 @@ SM.injectScript = function(src) {
     var script = document.createElement('script');
     script.src = Extension.getURL(src);
     return script;
-};
-
-SM.prepend = function (parent, child) {
-    if (parent.firstChild) {
-        parent.insertBefore(child, parent.firstChild);
-    } else {
-        parent.appendChild(child);
-    }
 };
