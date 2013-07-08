5d268f1e99a80005f2c1edb87b37cd59dee0e012
Maurice Lam <me@mauricelam.com>
2013-07-08 00:17:38 -0700

    Shim.js

--- Shim.js
+++ Shim.js
@@ -5,17 +5,32 @@ var Message = {};
     Message.tab = {};
 
     Message.tab.addListener = function (listener) {
+        chrome.extension.onMessage.addListener(function (message, sender, sendResponse) {
+            var action = message.action;
+            var data = message.data;
+            listener.call(this, action, data, sender, sendResponse);
+        });
     };
 
     Message.tab.sendMessage = function (target, action, data, responseCallback) {
+        var tabId = (typeof target === 'number') ? target : target.tab.id;
+        responseCallback = responseCallback || function () {};
+        chrome.tabs.sendMessage(tabId, { 'action': action, 'data': data }, responseCallback);
     };
 
     Message.extension = {};
 
     Message.extension.addListener = function (listener) {
+        chrome.extension.onMessage.addListener(function (message, sender, sendResponse) {
+            var action = message.action;
+            var data = message.data;
+            listener.call(this, action, data, sender, sendResponse);
+        });
     };
 
     Message.extension.sendMessage = function (action, data, responseCallback) {
+        responseCallback = responseCallback || function () {};
+        chrome.extension.sendMessage({ 'action': action, 'data': data }, responseCallback);
     };
 
 })();
@@ -25,9 +40,18 @@ var Extension = {};
 (function () {
 
     Extension.getURL = function (path) {
+        return chrome.extension.getURL(path);
     };
 
     Extension.forAllTabs = function (fn) {
+        chrome.windows.getAll({populate: true}, function(windows){
+            for(var i in windows){
+                var tabs = windows[i].tabs;
+                for(var j in tabs){
+                    fn(tabs[j]);
+                }
+            }
+        });
     };
 
 }());
