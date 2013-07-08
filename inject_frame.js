78fbde621a33bd24dda0c81f4313bcb9967bc743
Maurice Lam <me@mauricelam.com>
2013-07-08 00:04:55 -0700

    Support for new maps

--- inject_frame.js
+++ inject_frame.js
@@ -3,10 +3,26 @@
 var SM = SM || { count: 0 };
 SM.inframe = (window.top !== window);
 
+var retries = 3;
+
+function injectNewMaps() {
+    var elem = document.querySelectorAll('.widget-scene')[0];
+    if (elem) {
+        new ScrollableMap(elem, ScrollableMap.TYPE_NEWWEB, SM.count++);
+    } else if (retries > 0) {
+        // Retry a few times because the new map canvas is not installed on DOM load
+        retries--;
+        setTimeout(injectNewMaps, 1000);
+    }
+}
+
 function injectFrame() {
     // dont activate this thing at all if the frame has no map
-    if(document.getElementById('map')){
-        new ScrollableMap(document.getElementById('map'), (SM.inframe) ? ScrollableMap.TYPE_IFRAME : ScrollableMap.TYPE_WEB, SM.count++);
+    var elem = document.getElementById('map');
+    if (elem) {
+        new ScrollableMap(elem, (SM.inframe) ? ScrollableMap.TYPE_IFRAME : ScrollableMap.TYPE_WEB, SM.count++);
+    } else if (!SM.inframe) {
+        injectNewMaps();
     }
 }
 
