0b3293e96444d38209bbff23793020a74b5eca35
Maurice Lam <me@mauricelam.com>
2018-10-07 00:34:29 -0700

    Whitelist custom maps viewer for API injection

--- Gruntfile.js
+++ Gruntfile.js
@@ -219,7 +219,7 @@ const TEST_SITES = [
     'https://developers.google.com/maps/documentation/javascript/styling',
     'https://developers.google.com/maps/documentation/embed/guide',
     'http://maps.google.com/?force=tt',
-    'http://maps.google.be/',
+    // 'http://maps.google.be/',
     'https://developers.google.com/maps/documentation/javascript/signedin',
     'https://developers.google.com/maps/documentation/javascript/examples/polygon-draggable',
     'https://developers.google.com/maps/documentation/javascript/examples/layer-data-quakes',
