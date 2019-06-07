6400d60f7e1e9a2e2c056d4a15fb4da94982ca9e
Tarık Taha Uygun <ttuygun@yahoo.com>
2019-06-06 21:54:06 -0700

    Fix scrollability injection from parent frames

--- src/background.js
+++ src/background.js
@@ -1,13 +1,31 @@
 /*global Message */
 
+const DEBUG = false;
+
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
         if (details.type === 'script') {
+            if (DEBUG) {
+                console.log('Map API script requested', details);
+            }
+            if (details.tabId < 0) {
+                console.warn('Tab ID is less than 0', details);
+                return {};
+            }
             chrome.tabs.executeScript(details.tabId, {
                 'file': 'mapapi_inject.min.js',
                 'runAt': 'document_start',
                 'frameId': details.frameId
             });
+            chrome.tabs.executeScript(details.tabId, {
+                'file': 'scrollability_inject.min.js',
+                'runAt': 'document_idle',
+                'allFrames': true
+            });
+        } else {
+            if (DEBUG) {
+                console.log('Non matching request', details);
+            }
         }
         return {};
     },
