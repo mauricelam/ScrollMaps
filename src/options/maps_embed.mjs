950fc36d1bdf4220594b0b476bf8eb9e122ad971
Maurice Lam <me@mauricelam.com>
2024-01-23 22:19:56 -0800

    Update how API key is hidden

--- src/options/maps_embed.mjs
+++ src/options/maps_embed.mjs
@@ -1,1 +1,2 @@
-export const SCROLLMAPS_IFRAME_URL = 'https://www.google.com/maps/embed/v1/view?key=AIzaSyCs4QGENEbwHZlRhMfpu4Xq2pTlwaQvb9w&zoom=12&center=37.3861%2C-122.0839';
+// This URL needs to be overridden to include an API key.
+export const SCROLLMAPS_IFRAME_URL = `https://www.google.com/maps/embed/v1/view?zoom=12&center=37.3861%2C-122.0839`;
